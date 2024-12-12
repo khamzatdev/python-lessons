d732c65e0c5844aa3fed3929b13382d17443d5e2
dev_khushvaktov <168487927+khamzatdev@users.noreply.github.com>
2024-12-12 14:41:59 +0300

    yangi qushimcha

--- 13-dars.py.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Редактор Spyder
-
-Это временный скриптовый файл.
-"""
-
-#print('Odami ersang demagil odami,\nOniki yo\'q xa xalq g\'amidin g\'ami')
-#Uch tirnoq quyib yoziladigan bulsa unda matnni pastga tushirish uchun \n belgisi shart emas
